70af5d0b983a4f86358b3c381e695514b6f07b6c
Vaughan Jones <vlj7491@gmail.com>
2023-12-22 20:07:10 +1100

    colorscheme and treesitter config

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      -- Automatically install missing parsers when entering buffer.
+      auto_install = true,
+    },
+  },
+}

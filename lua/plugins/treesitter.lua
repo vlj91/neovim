b74dadeef67ce610c306d32ed7e494b6de66dfb7
Vaughan Jones <vlj7491@gmail.com>
2023-12-27 10:28:05 +1100

    Configures highlight and indent treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,8 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
       -- Automatically install missing parsers when entering buffer.
       auto_install = true,
     },

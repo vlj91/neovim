ee721c03653aab1dbb5b847b30c7924ce64b4fa0
Vaughan Jones <vlj7491@gmail.com>
2023-12-27 09:49:30 +1100

    switch to papercolor theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "NLKNguyen/papercolor-theme" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "papercolor",
     },
   },
 }
